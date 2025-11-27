f140cd79b95d2aed8d7e718f0c20297439667783
MaxamatO <pierchala.maks@gmail.com>
2025-11-27 22:24:35 +0100

    lcd initialized and printed text

--- main/lcd1602_drvr/lcd1602.c
+++ main/lcd1602_drvr/lcd1602.c
@@ -7,17 +7,24 @@
 #define BIT_E  (1 << 2)         // Starts data read/write
 
 static esp_err_t lcd1602_write_byte(lcd1602_t *lcd, uint8_t *data);
-static esp_err_t lcd1602_write_4bits(lcd1602_t *lcd, uint8_t *data);
+static esp_err_t lcd1602_write_4bits(lcd1602_t *lcd, uint8_t data, uint8_t mode);
 
 
 static esp_err_t lcd1602_write_byte(lcd1602_t *lcd, uint8_t *data){
+    return ESP_OK;
 
 }
 
-static esp_err_t lcd1602_write_4bits(lcd1602_t *lcd, uint8_t *data){
-    
+static esp_err_t lcd1602_write_4bits(lcd1602_t *lcd, uint8_t data, uint8_t mode){
+    // We want to send data as 00000000,
+    // Mode: 1 = Data register, 0 = Instruction register 
+    //uint8_t prepare_byte = (data & 0xF0) | 
+
+
+    return ESP_OK;
 }
 
 esp_err_t lcd1602_clear_display(lcd1602_t *lcd){
+    return ESP_OK;
 
 }
