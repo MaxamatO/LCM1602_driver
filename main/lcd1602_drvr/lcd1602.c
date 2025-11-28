de4cb080fca701d65cd7a3bf22dfd193a53a0712
MaxamatO <pierchala.maks@gmail.com>
2025-11-28 17:25:10 +0100

    Implement driver as external library

--- main/lcd1602_drvr/lcd1602.c
+++ main/lcd1602_drvr/lcd1602.c
@@ -1,30 +1,124 @@
 #include "lcd1602.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
+// General bit definitions
+#define ENABLE          0x04    // BIT_E
 
-#define BIT_RS (1 << 0)         // 1 = Data Register, 0 = Instruction Register
-#define BIT_RW (1 << 1)         // 1 = Read, 0 = Write
-#define BIT_E  (1 << 2)         // Starts data read/write
+#define COMMAND_MODE    0x00    // RS = 0
+#define DATA_MODE       0x01    // RS = 1
 
-static esp_err_t lcd1602_write_byte(lcd1602_t *lcd, uint8_t *data);
-static esp_err_t lcd1602_write_4bits(lcd1602_t *lcd, uint8_t data, uint8_t mode);
+#define WRITE_MODE      0x00    // RW = 0
+#define READ_MODE       0x02    // RW = 1
 
+#define BACKLIGHT_ON    0x08
+#define BACKLIGHT_OFF   0x00
 
-static esp_err_t lcd1602_write_byte(lcd1602_t *lcd, uint8_t *data){
-    return ESP_OK;
+// Commands for function use
+#define DISPLAY_ON      0x0C    
+#define DISPLAY_OFF     0x08
+#define CLEAR_DISPLAY   0x01
+#define RETURN_HOME     0x02
+#define CURSOR_ON       0x0B // 00001011
+#define CURSOR_OFF      0x09
+#define ENTRY_MODE_SET  0x06
+#define DISPLAY_CRUSOR  0x0E
 
+static void lcd1602_write_byte(lcd1602_t *lcd, uint8_t data, uint8_t mode);
+static void lcd1602_write_nibble(lcd1602_t *lcd, uint8_t nibble);
+static void lcd1602_write_command(lcd1602_t *lcd, uint8_t cmd);
+static void lcd1602_write_data(lcd1602_t *lcd, uint8_t data);
+// Initialization sequence for LCD1602 in 4-bit mode
+// According to the datasheet, we need to follow a specific sequence
+void lcd1602_init(lcd1602_t *lcd){
+    lcd->bl = BACKLIGHT_OFF; // Disable backlight during init to not messup init sequence
+	vTaskDelay(pdMS_TO_TICKS(50)); // Wait for more than 40ms after VCC rises to 2.7V
+
+    // Steps from documentation - 3 times 0x03
+    lcd1602_write_nibble(lcd, (0x03 << 4));
+    ets_delay_us(4200);
+
+	lcd1602_write_nibble(lcd, (0x03 << 4));
+    ets_delay_us(4200);
+
+    lcd1602_write_nibble(lcd, (0x03 << 4));
+    ets_delay_us(110);
+
+    lcd1602_write_nibble(lcd, 0x20);		// 4bit
+    clearDisplay(lcd);
+    returnHome(lcd);
+	lcd1602_write_command(lcd, 0x0C);		// return home
+    lcd->bl = BACKLIGHT_ON; // Disable backlight during init to not messup init sequence
 }
 
-static esp_err_t lcd1602_write_4bits(lcd1602_t *lcd, uint8_t data, uint8_t mode){
-    // We want to send data as 00000000,
-    // Mode: 1 = Data register, 0 = Instruction register 
-    //uint8_t prepare_byte = (data & 0xF0) | 
-
-
-    return ESP_OK;
+void clearDisplay(lcd1602_t *lcd){
+    lcd1602_write_command(lcd, CLEAR_DISPLAY);
+    ets_delay_us(3000);
 }
 
-esp_err_t lcd1602_clear_display(lcd1602_t *lcd){
-    return ESP_OK;
+void returnHome(lcd1602_t *lcd){
+    lcd1602_write_command(lcd, RETURN_HOME); 
+    ets_delay_us(3000);
+}
 
+void displayOn(lcd1602_t *lcd){
+    lcd1602_write_command(lcd, DISPLAY_ON);
+}
+
+void displayOff(lcd1602_t *lcd){
+    lcd1602_write_command(lcd, DISPLAY_OFF);
+}
+
+void cursorOn(lcd1602_t *lcd){
+    lcd1602_write_command(lcd, CURSOR_ON);
+}
+
+void cursorOff(lcd1602_t *lcd){
+    lcd1602_write_command(lcd, CURSOR_OFF);
+}
+
+void entryModeSet(lcd1602_t *lcd){
+    lcd1602_write_command(lcd, ENTRY_MODE_SET);
+}
+
+void write_char(lcd1602_t *lcd, char c){
+    lcd1602_write_data(lcd, (uint8_t)c);
+}
+
+void write_string(lcd1602_t *lcd, const char *str){
+    while(*str){
+        write_char(lcd, *str);
+        str++;
+    }
+}
+
+
+static void lcd1602_write_byte(lcd1602_t *lcd, uint8_t data, uint8_t mode){
+    uint8_t h_nibble = (data & 0xF0) | mode;
+	uint8_t l_nibble = ((data << 4) & 0xF0) | mode;
+	lcd1602_write_nibble(lcd, h_nibble);
+	lcd1602_write_nibble(lcd, l_nibble);
+}
+
+static void lcd1602_write_nibble(lcd1602_t *lcd, uint8_t nibble){
+    uint8_t data;
+
+	nibble = nibble | lcd->bl;
+
+	data = nibble | ENABLE;
+	i2c_master_transmit(lcd->dev, &data, sizeof(data), -1);
+	ets_delay_us(20);
+
+
+	data = nibble & ~ENABLE;
+	i2c_master_transmit(lcd->dev, &data, sizeof(data), -1);
+	ets_delay_us(300);
+}
+
+static void lcd1602_write_command(lcd1602_t *lcd, uint8_t cmd){
+    lcd1602_write_byte(lcd, cmd, COMMAND_MODE);
+}
+
+static void lcd1602_write_data(lcd1602_t *lcd, uint8_t data){
+    lcd1602_write_byte(lcd, data, DATA_MODE);
 }
--- main/lcd1602_drvr/lcd1602.h
+++ main/lcd1602_drvr/lcd1602.h
@@ -2,6 +2,8 @@
 #include "driver/i2c_master.h"
 #include "esp_err.h"
 #include "stdint.h"
+#include "rom/ets_sys.h"
+
 
 
 typedef struct {
@@ -9,6 +11,14 @@ typedef struct {
     uint8_t bl;
 }lcd1602_t;
 
-esp_err_t lcd1602_init(lcd1602_t *lcd);
-esp_err_t lcd1602_cursor_reset(lcd1602_t *lcd);
-esp_err_t lcd1602_clear_display(lcd1602_t *lcd);
+void lcd1602_init(lcd1602_t *lcd);
+void clearDisplay(lcd1602_t *lcd);
+void returnHome(lcd1602_t *lcd);
+void displayOn(lcd1602_t *lcd);
+void displayOff(lcd1602_t *lcd);
+void set_cursor(lcd1602_t *lcd, uint8_t row, uint8_t col);
+void write_string(lcd1602_t *lcd, const char *str);
+void write_char(lcd1602_t *lcd, char c);
+void cursorOn(lcd1602_t *lcd);
+void cursorOff(lcd1602_t *lcd);
+void entryModeSet(lcd1602_t *lcd);
